725e9a2a8b6de9b642e9a2cc17e9b26f3df644cc
Joe Jevnik <joe@quantopian.com>
2018-12-07 16:26:26 -0500

    STY: use nested namespace

--- include/libpy/meta.h
+++ include/libpy/meta.h
@@ -2,13 +2,12 @@
 
 #include <type_traits>
 
-namespace py {
-
 /** Utilities for metaprogramming.
  */
-namespace meta {
+namespace py ::meta {
 
-/** Boolean variable template for checking if type appears in the fields of a std::tuple.
+/** Boolean variable template for checking if type appears in the fields of a
+ * std::tuple.
  *
  *  @tparam Search The type to be searched for.
  *  @tparam Elements The types of parameters appearing in the tuple.
@@ -25,11 +24,10 @@ constexpr bool element_of;
 template<typename Search>
 constexpr bool element_of<Search, std::tuple<>> = false;
 
-// Recursive Case: Search is an element of tuple<Head, Rest...> if it's equal to Head or
-// if it's an element of tuple<Rest...>.
+// Recursive Case: Search is an element of tuple<Head, Rest...> if it's equal to Head
+// or if it's an element of tuple<Rest...>.
 template<typename Search, typename Head, typename... Rest>
 constexpr bool element_of<Search, std::tuple<Head, Rest...>> =
     std::is_same_v<Search, Head> || element_of<Search, std::tuple<Rest...>>;
 
-}  // namespace meta
-}  // namespace py
+}  // namespace py::meta
