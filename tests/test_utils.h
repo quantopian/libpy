a876fc3e091f76192497da1fbfca96d34e55aeb5
Joe Jevnik <joe@quantopian.com>
2019-01-07 19:38:17 -0500

    DEV: show the Python exception that made it to the end of the test

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -1,9 +1,41 @@
 #pragma once
 
+#include <string>
+#include <sstream>
+
 #include "Python.h"
 #include "gtest/gtest.h"
 
 class with_python_interpreter : public testing::Test {
+private:
+    std::string format_exception(PyObject* type, PyObject* value, PyObject*) {
+        std::stringstream buf;
+
+        if (!type) {
+            buf << "<unknown type>";
+        }
+        else {
+            buf << reinterpret_cast<PyTypeObject*>(type)->tp_name;
+        }
+
+        buf << ": ";
+
+        if (!value) {
+            buf << "<nullptr>";
+        }
+        else {
+            PyObject* as_string = PyObject_ASCII(value);
+            if (!as_string) {
+                buf << "<nullptr>";
+            }
+            else {
+                buf << PyUnicode_AsUTF8(as_string);
+                Py_DECREF(as_string);
+            }
+        }
+
+        return buf.str();
+    }
 public:
     static void SetUpTestCase() {
         // initialize the Python interpreter state
@@ -16,7 +48,12 @@ public:
     }
 
     virtual void TearDown() {
-        EXPECT_FALSE(PyErr_Occurred()) << "test ended with Python exception set";
-        PyErr_Clear();
+        PyObject* type;
+        PyObject* value;
+        PyObject* traceback;
+        PyErr_Fetch(&type, &value, &traceback);
+
+        EXPECT_FALSE(type || value || traceback)
+            << format_exception(type, value, traceback);
     }
 };
