055b8276bb665585a767686ef56f3a52b19e7c5b
Joe Jevnik <joe@quantopian.com>
2019-03-19 15:40:18 -0400

    ENH: add any_vector and improve any_ref operations with vtable

--- include/libpy/any.h
+++ include/libpy/any.h
@@ -2,7 +2,21 @@
 
 #include <any>
 
+#include "libpy/demangle.h"
+
 namespace py {
+struct any_ref_vtable_type {
+    std::size_t size;
+    void (*assign)(void* lhs, const void* rhs);
+    bool (*ne)(void* lhs, void* rhs);
+    bool (*eq)(void* lhs, void* rhs);
+    void (*default_construct)(void* dest);
+    void (*copy_construct)(void* dest, const void* value);
+    void (*move_construct)(void* dest, void* value);
+    py::util::demangled_cstring (*type_name)();
+};
+
+namespace detail {
 /** A unique function for dispatching to the `operator=` of another type.
 
     @tparam The type to dispatch to.
@@ -10,16 +24,36 @@ namespace py {
     @param rhs_addr The address of the right hand side of the assignment.
  */
 template<typename T>
-void any_ref_assign(void* lhs_addr, const void* rhs_addr) {
-    *static_cast<T*>(lhs_addr) = *static_cast<const T*>(rhs_addr);
-}
+constexpr any_ref_vtable_type any_ref_vtable_instance = {
+    sizeof(T),
+    [](void* lhs, const void* rhs) {
+        *static_cast<T*>(lhs) = *static_cast<const T*>(rhs);
+    },
+    [](void* lhs, void* rhs) -> bool {
+        return *static_cast<T*>(lhs) != *static_cast<const T*>(rhs);
+    },
+    [](void* lhs, void* rhs) -> bool {
+        return *static_cast<T*>(lhs) == *static_cast<const T*>(rhs);
+    },
+    [](void* dest) {
+        new(dest) T();
+    },
+    [](void* dest, const void* value) {
+        new(dest) T(*static_cast<const T*>(value));
+    },
+    [](void* dest, void* value) {
+        new(dest) T(std::move(*static_cast<T*>(value)));
+    },
+    []() {
+        return py::util::type_name<T>();
+    },
+};
+}  // namespace detail
 
-/** A function pointer to a specialization of `any_ref_assign`. This doubles
-    as the run time type information tag for any ref and view.
- */
-using any_ref_assign_func = void (*)(void*, const void*);
+template<typename T>
+constexpr const any_ref_vtable_type* any_ref_vtable = &detail::any_ref_vtable_instance<T>;
 
-/** A mutable dynamic reference to a value whose type which isn't known until runtime.
+/** A mutable dynamic reference to a value whose type isn't known until runtime.
 
     This object is like `std::any`, except it is *non-owning*. Assignment has
     reference semantics, meaning it will assign through to the referent.
@@ -29,7 +63,31 @@ using any_ref_assign_func = void (*)(void*, const void*);
 class any_ref {
 private:
     void* m_addr;
-    any_ref_assign_func m_assign;
+    const any_ref_vtable_type* m_vtable;
+
+    template<typename T>
+    void typecheck(const T&) const {
+        if (any_ref_vtable<T> != m_vtable) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    inline void typecheck(const any_ref& other) const {
+        if (m_vtable != other.m_vtable) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    template<typename T>
+    bool cmp(bool (*f)(void*, void*), const T& other) const {
+        typecheck(other);
+        if constexpr (std::is_same_v<T, any_ref>) {
+            return f(m_addr, other.m_addr);
+        }
+        else {
+            return f(m_addr, std::addressof(other));
+        }
+    }
 
 public:
     /** Construct an `any_ref` from some arbitrary address and an assignment
@@ -37,29 +95,36 @@ public:
         tag.
 
         @param addr The address of the referent.
-        @param assign The function to use to assign to `addr`. This should
-                      be a specialization of `any_ref_assign`.
+        @param vtable The vtable for the type of the referent.
      */
-    inline any_ref(void* addr, any_ref_assign_func assign)
-        : m_addr(addr), m_assign(assign) {}
+    inline any_ref(void* addr, const any_ref_vtable_type* vtable)
+        : m_addr(addr), m_vtable(vtable) {}
 
     inline any_ref& operator=(const any_ref& rhs) {
-        if (rhs.m_assign != m_assign) {
-            throw std::bad_any_cast{};
-        }
-
-        m_assign(m_addr, rhs.m_addr);
+        typecheck(rhs);
+        m_vtable->assign(m_addr, rhs.m_addr);
         return *this;
     }
 
     template<typename T>
-    inline any_ref& operator=(const T& rhs) {
-        if (&any_ref_assign<T> != m_assign) {
-            throw std::bad_any_cast{};
-        }
-
-        m_assign(m_addr, std::addressof(rhs));
+    any_ref& operator=(const T& rhs) {
+        typecheck(rhs);
+        m_vtable->assign(m_addr, std::addressof(rhs));
         return *this;
+    }
+
+    template<typename T>
+    bool operator!=(const T& rhs) const {
+        return cmp(m_vtable->ne, rhs);
+    }
+
+    template<typename T>
+    bool operator==(const T& rhs) const {
+        return cmp(m_vtable->eq, rhs);
+    }
+
+    const any_ref_vtable_type* vtable() const {
+        return m_vtable;
     }
 
     /** Cast the dynamic reference to a statically typed reference. If the
@@ -68,7 +133,7 @@ public:
      */
     template<typename T>
     T& cast() {
-        if (&any_ref_assign<T> != m_assign) {
+        if (any_ref_vtable<T> != m_vtable) {
             throw std::bad_any_cast{};
         }
 
@@ -81,11 +146,19 @@ public:
      */
     template<typename T>
     const T& cast() const {
-        if (&any_ref_assign<T> != m_assign) {
+        if (any_ref_vtable<T> != m_vtable) {
             throw std::bad_any_cast{};
         }
 
         return *static_cast<const T*>(m_addr);
+    }
+
+    inline void* addr() {
+        return m_addr;
+    }
+
+    inline const void* addr() const {
+        return m_addr;
     }
 };
 
@@ -96,10 +169,10 @@ public:
  */
 template<typename T>
 any_ref make_any_ref(T& ob) {
-    return {&ob, &any_ref_assign<T>};
+    return {&ob, any_ref_vtable<T>};
 }
 
-/** A constant dynamic reference to a value whose type which isn't known until runtime.
+/** A constant dynamic reference to a value whose type isn't known until runtime.
 
     This object is like `std::any`, except it is *non-owning*. The object is
     like a constant reference, and thus may not be assigned to.
@@ -109,7 +182,31 @@ any_ref make_any_ref(T& ob) {
 class any_cref {
 private:
     const void* m_addr;
-    any_ref_assign_func m_assign;
+    const any_ref_vtable_type* m_vtable;
+
+    template<typename T>
+    void typecheck(const T&) const {
+        if (any_ref_vtable<T> != m_vtable) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    inline void typecheck(const any_cref& other) const {
+        if (m_vtable != other.m_vtable) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    template<typename T>
+    bool cmp(bool (*f)(void*, void*), const T& other) const {
+        typecheck(other);
+        if constexpr (std::is_same_v<T, any_cref>) {
+            return f(m_addr, other.m_addr);
+        }
+        else {
+            return f(m_addr, std::addressof(other));
+        }
+    }
 
 public:
     /** Construct an `any_cref` from some arbitrary address and an assignment
@@ -117,11 +214,10 @@ public:
         tag, but will never be called (because the reference is immutable).
 
         @param addr The address of the referent.
-        @param assign The function to use to assign to `addr`. This should
-                      be a specialization of `any_ref_assign`.
+        @param vtable The vtable for the type of the referent.
      */
-    inline any_cref(const void* addr, any_ref_assign_func assign)
-        : m_addr(addr), m_assign(assign) {}
+    inline any_cref(const void* addr, const any_ref_vtable_type* vtable)
+        : m_addr(addr), m_vtable(vtable) {}
 
     // movable but not assignable
     any_cref(any_cref&&) = default;
@@ -129,17 +225,35 @@ public:
     any_cref(const any_cref&) = delete;
     any_cref& operator=(const any_cref&) = delete;
 
+    template<typename T>
+    bool operator!=(const T& rhs) const {
+        return cmp(m_vtable->ne, rhs);
+    }
+
+    template<typename T>
+    bool operator==(const T& rhs) const {
+        return cmp(m_vtable->eq, rhs);
+    }
+
+    const any_ref_vtable_type* vtable() const {
+        return m_vtable;
+    }
+
     /** Cast the dynamic reference to a statically typed reference. If the
         referred object is not of type `T`, and `std::bad_any_cast` will be
         thrown.
      */
     template<typename T>
     const T& cast() const {
-        if (&any_ref_assign<T> != m_assign) {
+        if (any_ref_vtable<T> != m_vtable) {
             throw std::bad_any_cast{};
         }
 
         return *static_cast<const T*>(m_addr);
+    }
+
+    inline const void* addr() const {
+        return m_addr;
     }
 };
 
@@ -150,6 +264,6 @@ public:
  */
 template<typename T>
 any_cref make_any_cref(T& ob) {
-    return {&ob, &any_ref_assign<T>};
+    return {&ob, any_ref_vtable<T>};
 }
 }  // namespace py
---
+++ include/libpy/any_vector.h
@@ -0,0 +1,300 @@
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+
+#include "libpy/any.h"
+
+namespace py {
+class any_vector {
+private:
+    const py::any_ref_vtable_type* m_vtable;
+    std::vector<char> m_storage;
+
+    std::ptrdiff_t pos_to_index(std::ptrdiff_t pos) const {
+        std::size_t ix;
+        if (__builtin_mul_overflow(pos, m_vtable->size, &ix)) {
+            throw std::overflow_error("pos * m_strides overflows std::size_t");
+        }
+
+        return ix;
+    }
+
+    template<typename T>
+    void typecheck(const T&) const {
+        if (&any_ref_vtable<T> != m_vtable) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    inline void typecheck(const any_ref& other) const {
+        if (m_vtable != other.vtable()) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    inline void typecheck(const any_cref& other) const {
+        if (m_vtable != other.vtable()) {
+            throw std::bad_any_cast{};
+        }
+    }
+
+    template<typename R, typename C>
+    struct generic_iterator {
+    protected:
+        friend any_vector;
+
+        char* m_ptr;
+        std::size_t m_ix;
+        std::int64_t m_stride;
+        const any_ref_vtable_type* m_vtable;
+
+        generic_iterator(char* buffer,
+                         std::size_t ix,
+                         std::int64_t stride,
+                         const any_ref_vtable_type* vtable)
+            : m_ptr(buffer), m_ix(ix), m_stride(stride), m_vtable(vtable) {}
+
+    public:
+        using difference_type = std::ptrdiff_t;
+        using value_type = C;
+        using pointer = C*;
+        using reference = R;
+        using const_reference = C;
+        using iterator_category = std::random_access_iterator_tag;
+
+        reference operator*() {
+            return {m_ptr, m_vtable};
+        }
+
+        const_reference operator*() const {
+            return {m_ptr, m_vtable};
+        }
+
+        reference operator[](difference_type ix) {
+            return *(*this + ix);
+        }
+
+        const_reference operator[](difference_type ix) const {
+            return *(*this + ix);
+        }
+
+        generic_iterator& operator++() {
+            m_ptr += m_stride;
+            m_ix += 1;
+            return *this;
+        }
+
+        generic_iterator operator++(int) {
+            generic_iterator out = *this;
+            m_ptr += m_stride;
+            m_ix += 1;
+            return out;
+        }
+
+        generic_iterator& operator+=(difference_type n) {
+            m_ptr += m_stride * n;
+            m_ix += n;
+            return *this;
+        }
+
+        generic_iterator operator+(difference_type n) const {
+            return {m_ptr + n * m_stride, m_ix + n, m_stride, m_vtable};
+        }
+
+        generic_iterator& operator--() {
+            m_ptr -= m_stride;
+            m_ix -= 1;
+            return *this;
+        }
+
+        generic_iterator operator--(int) {
+            generic_iterator out = *this;
+            m_ptr -= m_stride;
+            m_ix -= 1;
+            return out;
+        }
+
+        generic_iterator& operator-=(difference_type n) {
+            m_ptr -= m_stride * n;
+            m_ix -= n;
+            return *this;
+        }
+
+        difference_type operator-(const generic_iterator& other) const {
+            return m_ix - other.m_ix;
+        }
+
+        bool operator!=(const generic_iterator& other) const {
+            return !(m_ix == other.m_ix && m_ptr == other.m_ptr);
+        }
+
+        bool operator==(const generic_iterator& other) const {
+            return m_ix == other.m_ix && m_ptr == other.m_ptr;
+        }
+
+        bool operator<(const generic_iterator& other) const {
+            return m_ix < other.m_ix;
+        }
+
+        bool operator<=(const generic_iterator& other) const {
+            return m_ix <= other.m_ix;
+        }
+
+        bool operator>(const generic_iterator& other) const {
+            return m_ix > other.m_ix;
+        }
+
+        bool operator>=(const generic_iterator& other) const {
+            return m_ix >= other.m_ix;
+        }
+    };
+
+
+public:
+    any_vector() = delete;
+    inline any_vector(const any_ref_vtable_type* vtable) : m_vtable(vtable) {}
+    inline any_vector(const any_ref_vtable_type* vtable, std::size_t size)
+        : m_vtable(vtable), m_storage(size * vtable->size) {
+        for (std::size_t pos = 0; pos < this->size(); ++pos) {
+            vtable->default_construct(&m_storage[pos_to_index(pos)]);
+        }
+    }
+
+    template<typename T>
+    inline any_vector(const any_ref_vtable_type* vtable, std::size_t size, const T& value)
+        : m_vtable(vtable), m_storage(size * vtable->size) {
+        typecheck(value);
+        for (std::size_t pos = 0; pos < this->size(); ++pos) {
+            auto addr = &m_storage[pos_to_index(pos)];
+            if constexpr (std::is_same_v<T, any_ref> || std::is_same_v<T, any_cref>) {
+                m_vtable->copy_construct(addr, value.addr());
+            }
+            else {
+                m_vtable->copy_construct(addr, std::addressof(value));
+            }
+        }
+    }
+
+    using reference = any_ref;
+    using const_reference = any_cref;
+    using iterator = generic_iterator<any_ref, any_cref>;
+    using reverse_iterator = iterator;
+    using const_iterator = generic_iterator<any_cref, any_cref>;
+    using const_reverse_iterator = const_iterator;
+
+    inline iterator begin() {
+        return {m_storage.data(), 0, static_cast<std::int64_t>(m_vtable->size), m_vtable};
+    }
+
+    inline const_iterator begin() const {
+        return {const_cast<char*>(m_storage.data()),
+                0,
+                static_cast<std::int64_t>(m_vtable->size),
+                m_vtable};
+    }
+
+    inline iterator end() {
+        return {m_storage.data(), size(), static_cast<std::int64_t>(m_vtable->size), m_vtable};
+    }
+
+    inline const_iterator end() const {
+        return {const_cast<char*>(m_storage.data()),
+                size(),
+                static_cast<std::int64_t>(m_vtable->size),
+                m_vtable};
+    }
+
+    inline std::size_t size() const {
+        return m_storage.size() / m_vtable->size;
+    }
+
+    inline reference operator[](std::ptrdiff_t pos) {
+        return {&m_storage[pos_to_index(pos)], m_vtable};
+    }
+
+    inline reference at(std::ptrdiff_t pos) {
+        if (pos < 0 || static_cast<size_t>(pos) >= m_storage.size() / m_vtable->size) {
+            throw std::out_of_range("pos out of bounds");
+        }
+
+        return (*this)[pos];
+    }
+
+    inline const_reference operator[](std::ptrdiff_t pos) const {
+        return {&m_storage[pos_to_index(pos)], m_vtable};
+    }
+
+    inline const_reference at(std::ptrdiff_t pos) const {
+        if (pos < 0 || static_cast<size_t>(pos) >= size()) {
+            throw std::out_of_range("pos out of bounds");
+        }
+
+        return (*this)[pos];
+    }
+
+    inline reference front() {
+        return (*this)[0];
+    }
+
+    inline const_reference front() const {
+        return (*this)[0];
+    }
+
+    inline reference back() {
+        return (*this)[size() - 1];
+    }
+
+    inline const_reference back() const {
+        return (*this)[size() - 1];
+    }
+
+    inline void clear() {
+        m_storage.clear();
+    }
+
+    template<typename T>
+    void push_back(const T& value) {
+        typecheck(value);
+        void* addr = &m_storage.back();
+        m_storage.insert(m_storage.end(), m_vtable->size, ' ');
+        if constexpr (std::is_same_v<T, any_ref> || std::is_same_v<T, any_cref>) {
+            m_vtable->copy_construct(addr, value.addr());
+        }
+        else {
+            m_vtable->copy_construct(addr, std::addressof(value));
+        }
+    }
+
+    template<typename T>
+    void push_back(T&& value) {
+        typecheck(value);
+        void* addr = &m_storage.back();
+        m_storage.insert(m_storage.end(), m_vtable->size, ' ');
+        if constexpr (std::is_same_v<T, any_ref> || std::is_same_v<T, any_cref>) {
+            m_vtable->move_construct(addr, value.addr());
+        }
+        else {
+            m_vtable->move_construct(addr, std::addressof(value));
+        }
+    }
+
+    inline void pop_back() {
+        for (std::size_t b = 0; b < m_vtable->size; ++b) {
+            m_storage.pop_back();
+        }
+    }
+
+    inline void* data() noexcept {
+        return m_storage.data();
+    }
+
+    inline const void* data() const noexcept {
+        return m_storage.data();
+    }
+
+    inline const any_ref_vtable_type* vtable() const {
+        return m_vtable;
+    }
+};
+}  // namespace py
