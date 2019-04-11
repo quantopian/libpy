22e76c0f55009bfe937a5ea53adcc12d6fff18ea
Joe Jevnik <joe@quantopian.com>
2019-04-11 16:04:52 -0400

    DEV: fix make format

--- tests/main.cc
+++ tests/main.cc
@@ -2,7 +2,6 @@
 #include <Python.h>
 
 #define LIBPY_TEST_MAIN
-#define PY_ARRAY_UNIQUE_SYMBOL PyArray_API_libpy
 #include "libpy/numpy_utils.h"
 
 int main(int argc, char** argv) {
@@ -10,7 +9,7 @@ int main(int argc, char** argv) {
     py::ensure_import_array();
 
     testing::InitGoogleTest(&argc, argv);
-    int out =  RUN_ALL_TESTS();
+    int out = RUN_ALL_TESTS();
     Py_Finalize();
     return out;
 }
