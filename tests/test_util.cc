5b1ef4e61404b2a88169351c675ef92503236ad0
Ernesto Perez <ernesto@quantopian.com>
2019-06-07 09:24:36 -0400

    TST: Test searchsorted utils.

---
+++ tests/test_util.cc
@@ -0,0 +1,53 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "libpy/util.h"
+
+namespace test_util {
+    using namespace py::util;
+
+    TEST(searchsorted_left, all) {
+        std::vector<std::int64_t> vec{1, 2, 3, 5, 6};
+
+        // needle not in container.
+        EXPECT_EQ(searchsorted_l(vec, 4), 3);
+
+        // needle collides with value in container.
+        EXPECT_EQ(searchsorted_l(vec, 3), 2);
+
+        // needle greater than largest value in container.
+        EXPECT_EQ(searchsorted_l(vec, 42), vec.size());
+
+        // needle less than smallest value in container.
+        EXPECT_EQ(searchsorted_l(vec, -1), 0);
+
+        // needle equal to largest value in container.
+        EXPECT_EQ(searchsorted_l(vec, 6), 4);
+
+        // needle equal to smallest value in container.
+        EXPECT_EQ(searchsorted_l(vec, 1), 0);
+    }
+
+    TEST(searchsorted_right, all) {
+        std::vector<std::int64_t> vec{1, 2, 3, 5, 6};
+
+        // needle not in container.
+        EXPECT_EQ(searchsorted_r(vec, 4), 3);
+
+        // needle collides with value in container.
+        EXPECT_EQ(searchsorted_r(vec, 3), 3);
+
+        // needle greater than largest value in container.
+        EXPECT_EQ(searchsorted_r(vec, 42), vec.size());
+
+        // needle less than smallest value in container.
+        EXPECT_EQ(searchsorted_r(vec, -1), 0);
+
+        // needle equal to largest value in container.
+        EXPECT_EQ(searchsorted_r(vec, 6), vec.size());
+
+        // needle equal to smallest value in container.
+        EXPECT_EQ(searchsorted_r(vec, 1), 1);
+    }
+}  // namespace test_util
