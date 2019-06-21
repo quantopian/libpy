db645a2c018ba172a2bac3028ac8721e2a0682d6
Joe Jevnik <joe@quantopian.com>
2019-06-21 17:49:04 -0400

    ENH: add nwise_devirtualize for getting statically typed access to array_view<any>

---
+++ include/libpy/devirtualize.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+
+#include "libpy/meta.h"
+#include "libpy/ndarray_view.h"
+
+namespace py {
+/** Interface to provide typed access to a collection of type-erased array_views.
+
+    @tparam arity The number of arrays to access at one time.
+    @tparam Ts The types to provided optimized access through. If `py::any_ref` or
+            `py::any_cref` appears in the list, it will catch any type of array_view.
+ */
+template<std::size_t arity, typename... Ts>
+class nwise_devirtualize {
+private:
+    static_assert(arity > 0, "arity must be non-zero");
+
+    template<typename T>
+    using views = std::array<py::array_view<T>, arity>;
+
+    template<typename T>
+    using type_entry = std::pair<views<T>, std::size_t>;
+
+    std::tuple<std::vector<type_entry<Ts>>...> m_arrays;
+
+    template<std::size_t tuple_ix, typename Head, typename... Tail>
+    void add_typed_entry(const views<py::any_ref>& args, std::size_t ix) {
+        if constexpr (std::is_same_v<Head, py::any_ref> ||
+                      std::is_same_v<Head, py::any_cref>) {
+
+            std::get<tuple_ix>(m_arrays).emplace_back(args, ix);
+        }
+        else if (std::get<0>(args).vtable() == py::any_vtable::make<Head>()) {
+            std::get<tuple_ix>(m_arrays).emplace_back(
+                std::apply(
+                    [](const auto&... untyped) {
+                        return views<Head>{untyped.template cast<Head>()...};
+                    },
+                    args),
+                ix);
+        }
+        else {
+            add_typed_entry<tuple_ix + 1, Tail...>(args, ix);
+        }
+    }
+
+    template<std::size_t>
+    void add_typed_entry(const views<py::any_ref>&, std::size_t) {
+        throw std::bad_any_cast{};
+    }
+
+    void add_entry(const views<py::any_ref>& args, std::size_t ix) {
+        if (!std::apply([](auto... args) { return util::all_equal(args.vtable()...); },
+                        args)) {
+            throw std::bad_any_cast{};
+        }
+
+        add_typed_entry<0, Ts...>(args, ix);
+    }
+
+public:
+    /** Construct an `nwise_devirtualize` from `arity` collections of
+        `py::array_view<py::any_ref>` or `py::array_view<py::any_cref>`.
+
+        @param array_collections One sequence of array views per `arity`.
+     */
+    template<typename... Args>
+    nwise_devirtualize(const Args&... array_collections) {
+        static_assert(sizeof...(Args) == arity, "Size of array_collections != arity");
+
+        if (!util::all_equal(array_collections.size()...)) {
+            throw std::invalid_argument{"array collections are not all the same size"};
+        }
+
+        std::size_t max_size = std::get<0>(std::make_tuple(array_collections...)).size();
+        for (std::size_t ix = 0; ix < max_size; ++ix) {
+            views<py::any_ref> items{array_collections[ix]...};
+            add_entry(items, ix);;
+        }
+    }
+
+private:
+    template<typename F, typename T>
+    void for_each_helper(F&& f, const std::vector<type_entry<T>>& entries) {
+        for (const auto& [views, ix] : entries) {
+            std::apply(f, views);
+        }
+    }
+
+public:
+    /** Call `f` on each set of arrays but pass the arrays with their static type.
+
+        `f` is not guaranteed to be called on the arrays in the order they were passed
+        to the constructor. If you would like to know the original array index of the
+        arguments, use `for_each_with_ix`.
+
+        @param f A function object which can be called with with a signature of:
+               `f(const py::array_view<T>&...)` for each `T` in `Ts` and with an arity
+               that matches `arity`.
+     */
+    template<typename F>
+    void for_each(F&& f) {
+        std::apply(
+            [&](const auto&... args) {
+                (for_each_helper(std::forward<F>(f), args), ...);
+            },
+            m_arrays);
+    }
+
+private:
+    template<typename F, typename T>
+    void for_each_with_ix_helper(F&& f, const std::vector<type_entry<T>>& entries) {
+        for (const auto& [views, ix] : entries) {
+            std::apply([&](const auto&... args) { f(ix, args...); }, views);
+        }
+    }
+
+public:
+    /** Call `f` on each set of arrays but pass the arrays with their static type.
+
+        `f` is not guaranteed to be called on the arrays in the order they were passed
+        to the constructor. The `ix` argument provided will represent the index of the
+        array views from the original constructor call.
+
+        @param f A function object which can be called with with a signature of:
+               `f(std::size_t ix, const py::array_view<T>&...)` for each `T` in `Ts` and
+               with an arity that matches `arity`.
+     */
+    template<typename F>
+    void for_each_with_ix(F&& f) {
+        std::apply(
+            [&](const auto&... args) {
+                (for_each_with_ix_helper(std::forward<F>(f), args), ...);
+            },
+            m_arrays);
+    }
+};
+
+template<typename... Ts>
+using devirtualize = nwise_devirtualize<1, Ts...>;
+}  // namespace py
---
+++ tests/test_devirtualize.cc
@@ -0,0 +1,268 @@
+#include <array>
+#include <ostream>
+#include <unordered_map>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "libpy/devirtualize.h"
+#include "libpy/ndarray_view.h"
+#include "libpy/hash.h"
+
+namespace test_devirtualize {
+/** Hashable pair of `std::byte*`.
+ */
+struct byteptr_pair {
+    std::byte* first;
+    std::byte* second;
+
+    bool operator==(const byteptr_pair& other) const {
+        return first == other.first && second == other.second;
+    }
+
+    bool operator!=(const byteptr_pair& other) const {
+        return !(*this == other);
+    }
+};
+}  // namespace test_devirtualize
+
+namespace std {
+template<>
+struct hash<test_devirtualize::byteptr_pair> {
+    std::size_t operator()(const test_devirtualize::byteptr_pair& p) const noexcept {
+        return py::hash_many(p.first, p.second);
+    }
+};
+}
+
+namespace test_devirtualize {
+TEST(devirtualize, for_each) {
+    std::vector<int> a = {1, 2, 3};
+    std::byte* a_data = reinterpret_cast<std::byte*>(a.data());
+
+    std::vector<float> b = {1.5, 2.5, 3.5};
+    std::byte* b_data = reinterpret_cast<std::byte*>(b.data());
+
+    std::vector<int> c = {4, 5, 6};
+    std::byte* c_data = reinterpret_cast<std::byte*>(c.data());
+
+    std::vector<py::array_view<py::any_ref>> virtualized = {a, b, c};
+
+    py::devirtualize<int, float> devirtualized(virtualized);
+
+    std::vector<std::byte*> int_seen;
+    std::vector<std::byte*> expected_int_seen = {a_data, c_data};
+    std::vector<std::byte*> float_seen;
+    std::vector<std::byte*> expected_float_seen = {b_data};
+    int called = 0;
+
+    devirtualized.for_each([&](const auto& arr) {
+        using T = decltype(arr);
+        if constexpr (std::is_same_v<T, const py::array_view<int>&>) {
+            int_seen.emplace_back(arr.buffer());
+        }
+        else if constexpr (std::is_same_v<T, const py::array_view<float>&>) {
+            float_seen.emplace_back(arr.buffer());
+        }
+        else {
+            static_assert(!std::is_same_v<T, T>, "unknown view type");
+        }
+
+        ++called;
+    });
+
+    EXPECT_EQ(int_seen, expected_int_seen);
+    EXPECT_EQ(float_seen, expected_float_seen);
+    EXPECT_EQ(called, 3);
+}
+
+TEST(devirtualize, binary_for_each) {
+    std::vector<int> a0 = {1, 2, 3};
+    std::byte* a0_data = reinterpret_cast<std::byte*>(a0.data());
+
+    std::vector<float> b0 = {1.5, 2.5, 3.5};
+    std::byte* b0_data = reinterpret_cast<std::byte*>(b0.data());
+
+    std::vector<int> c0 = {4, 5, 6};
+    std::byte* c0_data = reinterpret_cast<std::byte*>(c0.data());
+
+    std::vector<int> a1 = {10, 20, 30};
+    std::byte* a1_data = reinterpret_cast<std::byte*>(a1.data());
+
+    std::vector<float> b1 = {10.5, 20.5, 30.5};
+    std::byte* b1_data = reinterpret_cast<std::byte*>(b1.data());
+
+    std::vector<int> c1 = {40, 50, 60};
+    std::byte* c1_data = reinterpret_cast<std::byte*>(c1.data());
+
+    std::vector<py::array_view<py::any_ref>> a_virt = {a0, b0, c0};
+    std::vector<py::array_view<py::any_ref>> b_virt = {a1, b1, c1};
+
+    py::nwise_devirtualize<2, int, float> devirtualized{a_virt, b_virt};
+
+    std::vector<std::array<std::byte*, 2>> int_seen;
+    std::vector<std::array<std::byte*, 2>> expected_int_seen = {{a0_data, a1_data},
+                                                                {c0_data, c1_data}};
+    std::vector<std::array<std::byte*, 2>> float_seen;
+    std::vector<std::array<std::byte*, 2>> expected_float_seen = {{b0_data, b1_data}};
+    int called = 0;
+
+    devirtualized.for_each([&](const auto& arr0, const auto& arr1) {
+        using T0 = decltype(arr0);
+        using T1 = decltype(arr1);
+        testing::StaticAssertTypeEq<T0, T1>();
+
+        if constexpr (std::is_same_v<T0, const py::array_view<int>&>) {
+            int_seen.push_back({arr0.buffer(), arr1.buffer()});
+        }
+        else if constexpr (std::is_same_v<T0, const py::array_view<float>&>) {
+            float_seen.push_back({arr0.buffer(), arr1.buffer()});
+        }
+        else {
+            static_assert(!std::is_same_v<T0, T0>, "unknown view type");
+        }
+
+        ++called;
+    });
+
+    EXPECT_EQ(int_seen, expected_int_seen);
+    EXPECT_EQ(float_seen, expected_float_seen);
+    EXPECT_EQ(called, 3);
+}
+
+TEST(devirtualize, for_each_with_ix) {
+    std::vector<int> a = {1, 2, 3};
+    std::byte* a_data = reinterpret_cast<std::byte*>(a.data());
+
+    std::vector<float> b = {1.5, 2.5, 3.5};
+    std::byte* b_data = reinterpret_cast<std::byte*>(b.data());
+
+    std::vector<int> c = {4, 5, 6};
+    std::byte* c_data = reinterpret_cast<std::byte*>(c.data());
+
+    std::vector<py::array_view<py::any_ref>> virtualized = {a, b, c};
+
+    py::devirtualize<int, float> devirtualized(virtualized);
+
+    std::unordered_map<std::byte*, std::size_t> seen_ix;
+    std::unordered_map<std::byte*, std::size_t> expected_ix = {{a_data, 0},
+                                                               {b_data, 1},
+                                                               {c_data, 2}};
+
+    std::vector<std::byte*> int_seen;
+    std::vector<std::byte*> expected_int_seen = {a_data, c_data};
+    std::vector<std::byte*> float_seen;
+    std::vector<std::byte*> expected_float_seen = {b_data};
+    int called = 0;
+
+    devirtualized.for_each_with_ix([&](std::size_t ix, const auto& arr) {
+        using T = decltype(arr);
+        if constexpr (std::is_same_v<T, const py::array_view<int>&>) {
+            int_seen.emplace_back(arr.buffer());
+        }
+        else if constexpr (std::is_same_v<T, const py::array_view<float>&>) {
+            float_seen.emplace_back(arr.buffer());
+        }
+        else {
+            static_assert(!std::is_same_v<T, T>, "unknown view type");
+        }
+
+        seen_ix[arr.buffer()] = ix;
+        ++called;
+    });
+
+    EXPECT_EQ(int_seen, expected_int_seen);
+    EXPECT_EQ(float_seen, expected_float_seen);
+    EXPECT_EQ(called, 3);
+    EXPECT_EQ(seen_ix, expected_ix);
+}
+
+TEST(devirtualize, binary_for_each_with_ix) {
+    std::vector<int> a0 = {1, 2, 3};
+    std::byte* a0_data = reinterpret_cast<std::byte*>(a0.data());
+
+    std::vector<float> b0 = {1.5, 2.5, 3.5};
+    std::byte* b0_data = reinterpret_cast<std::byte*>(b0.data());
+
+    std::vector<int> c0 = {4, 5, 6};
+    std::byte* c0_data = reinterpret_cast<std::byte*>(c0.data());
+
+    std::vector<int> a1 = {10, 20, 30};
+    std::byte* a1_data = reinterpret_cast<std::byte*>(a1.data());
+
+    std::vector<float> b1 = {10.5, 20.5, 30.5};
+    std::byte* b1_data = reinterpret_cast<std::byte*>(b1.data());
+
+    std::vector<int> c1 = {40, 50, 60};
+    std::byte* c1_data = reinterpret_cast<std::byte*>(c1.data());
+
+    std::vector<py::array_view<py::any_ref>> a_virt = {a0, b0, c0};
+    std::vector<py::array_view<py::any_ref>> b_virt = {a1, b1, c1};
+
+    py::nwise_devirtualize<2, int, float> devirtualized{a_virt, b_virt};
+
+    std::unordered_map<byteptr_pair, std::size_t> seen_ix;
+    std::unordered_map<byteptr_pair, std::size_t> expected_ix = {
+        {{a0_data, a1_data}, 0},
+        {{b0_data, b1_data}, 1},
+        {{c0_data, c1_data}, 2},
+    };
+
+    std::vector<std::array<std::byte*, 2>> int_seen;
+    std::vector<std::array<std::byte*, 2>> expected_int_seen = {{a0_data, a1_data},
+                                                                {c0_data, c1_data}};
+    std::vector<std::array<std::byte*, 2>> float_seen;
+    std::vector<std::array<std::byte*, 2>> expected_float_seen = {{b0_data, b1_data}};
+    int called = 0;
+
+    devirtualized.for_each_with_ix(
+        [&](std::size_t ix, const auto& arr0, const auto& arr1) {
+            using T0 = decltype(arr0);
+            using T1 = decltype(arr1);
+            testing::StaticAssertTypeEq<T0, T1>();
+
+            if constexpr (std::is_same_v<T0, const py::array_view<int>&>) {
+                int_seen.push_back({arr0.buffer(), arr1.buffer()});
+            }
+            else if constexpr (std::is_same_v<T0, const py::array_view<float>&>) {
+                float_seen.push_back({arr0.buffer(), arr1.buffer()});
+            }
+            else {
+                static_assert(!std::is_same_v<T0, T0>, "unknown view type");
+            }
+
+            seen_ix[{arr0.buffer(), arr1.buffer()}] = ix;
+            ++called;
+        });
+
+    EXPECT_EQ(int_seen, expected_int_seen);
+    EXPECT_EQ(float_seen, expected_float_seen);
+    EXPECT_EQ(called, 3);
+    EXPECT_EQ(seen_ix, expected_ix);
+}
+
+TEST(devirtualize, unknown_type) {
+    std::vector<int> a;
+    std::vector<float> b;
+    std::vector<int> c;
+
+    std::vector<py::array_view<py::any_ref>> virtualized = {a, b, c};
+
+    EXPECT_THROW(({ py::devirtualize<int, double> devirtualized{virtualized}; }),
+                 std::bad_any_cast);
+}
+
+TEST(devirtualize, mismatched_size) {
+    std::vector<int> a;
+    std::vector<int> b;
+    std::vector<int> c;
+
+    std::vector<py::array_view<py::any_ref>> a_virt = {a};
+    std::vector<py::array_view<py::any_ref>> b_virt = {b, c};
+
+    EXPECT_THROW(({
+                     py::nwise_devirtualize<2, int> devirtualized{a_virt, b_virt};
+                 }),
+                 std::invalid_argument);
+}
+}  // namespace test_devirtualize
