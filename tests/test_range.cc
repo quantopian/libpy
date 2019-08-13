8c04d52d131afcac909db86268cedfeb6064f174
Joe Jevnik <joe@quantopian.com>
2019-08-13 18:51:16 -0400

    ENH: add a py::range function for iterating Python objects

---
+++ tests/test_range.cc
@@ -0,0 +1,95 @@
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "libpy/detail/python.h"
+#include "libpy/range.h"
+#include "libpy/exception.h"
+#include "test_utils.h"
+
+namespace test_dict_range {
+using namespace std::literals;
+
+class range : public with_python_interpreter {};
+
+TEST_F(range, iteration) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        it_0 = []
+        it_1 = [1, 2, 3]
+        it_2 = (1, 2, 3)
+
+        def gen():
+            yield 1
+            yield 2
+            yield 3
+
+        it_3 = gen()
+    )");
+    ASSERT_TRUE(ns);
+
+    {
+        PyObject* it_0 = PyDict_GetItemString(ns.get(), "it_0");
+        ASSERT_TRUE(it_0);
+
+        for ([[maybe_unused]] auto item : py::range(it_0)) {
+            FAIL() << "empty sequence should not enter the loop";
+        }
+    }
+
+    {
+        std::vector<std::int64_t> expected = {1, 2, 3};
+
+        for (const auto name : {"it_1", "it_2", "it_3"}) {
+            PyObject* it = PyDict_GetItemString(ns.get(), name);
+            ASSERT_TRUE(it);
+
+            std::vector<std::int64_t> actual;
+
+            for (auto value : py::range(it)) {
+                actual.emplace_back(py::from_object<std::int64_t>(value));
+            }
+
+            EXPECT_EQ(actual, expected);
+        }
+    }
+}
+
+TEST_F(range, not_iterable) {
+    auto ob = py::to_object(1);
+    ASSERT_TRUE(ob);
+
+    EXPECT_THROW(py::range{ob}, py::exception);
+    expect_pyerr_type_and_message(PyExc_TypeError, "'int' object is not iterable");
+    PyErr_Clear();
+}
+
+TEST_F(range, exception_in_next) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        def gen():
+            yield 1
+            yield 2
+            raise ValueError('die, you evil C++ program')
+
+        it = gen()
+    )");
+    ASSERT_TRUE(ns);
+
+    PyObject* it = PyDict_GetItemString(ns.get(), "it");
+    ASSERT_TRUE(it);
+
+    std::vector<std::int64_t> expected = {1, 2};
+    std::vector<std::int64_t> actual;
+    EXPECT_THROW(
+        {
+            for (auto value : py::range(it)) {
+                actual.emplace_back(py::from_object<std::int64_t>(value));
+            }
+        },
+        py::exception);
+    expect_pyerr_type_and_message(PyExc_ValueError, "die, you evil C++ program");
+    PyErr_Clear();
+
+    EXPECT_EQ(actual, expected);
+}
+}  // namespace test_dict_range
