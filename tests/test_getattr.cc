a2bb57750ee5fd726f9a3c532e219c1bcc6b700a
Joe Jevnik <joe@quantopian.com>
2019-08-13 19:44:19 -0400

    ENH: add getattr and nested_getattr helper

---
+++ tests/test_getattr.cc
@@ -0,0 +1,96 @@
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "libpy/detail/python.h"
+#include "libpy/getattr.h"
+#include "test_utils.h"
+
+namespace test_getattr {
+class getattr : public with_python_interpreter {};
+
+TEST_F(getattr, simple) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        class A(object):
+            b = 1
+
+        expected = A.b
+    )");
+    ASSERT_TRUE(ns);
+
+    PyObject* A = PyDict_GetItemString(ns.get(), "A");
+    ASSERT_TRUE(A);
+
+    py::scoped_ref<> actual = py::getattr(A, "b");
+    ASSERT_TRUE(actual);
+
+    PyObject* expected = PyDict_GetItemString(ns.get(), "expected");
+    ASSERT_TRUE(actual);
+
+    // compare them using object identity
+    EXPECT_EQ(actual.get(), expected);
+}
+
+TEST_F(getattr, attribute_error) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        class A(object):
+            pass
+    )");
+    ASSERT_TRUE(ns);
+
+    PyObject* A = PyDict_GetItemString(ns.get(), "A");
+    ASSERT_TRUE(A);
+
+    py::scoped_ref<> actual = py::getattr(A, "b");
+    ASSERT_FALSE(actual);
+    expect_pyerr_type_and_message(PyExc_AttributeError,
+                                  "type object 'A' has no attribute 'b'");
+    PyErr_Clear();
+}
+
+TEST_F(getattr, nested) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        class A(object):
+            class B(object):
+                class C(object):
+                    d = 1
+
+        expected = A.B.C.d
+    )");
+    ASSERT_TRUE(ns);
+
+    PyObject* A = PyDict_GetItemString(ns.get(), "A");
+    ASSERT_TRUE(A);
+
+    py::scoped_ref<> actual = py::nested_getattr(A, "B", "C", "d");
+    ASSERT_TRUE(actual);
+
+    PyObject* expected = PyDict_GetItemString(ns.get(), "expected");
+    ASSERT_TRUE(actual);
+
+    // compare them using object identity
+    EXPECT_EQ(actual.get(), expected);
+}
+
+TEST_F(getattr, nested_failure) {
+    py::scoped_ref ns = RUN_PYTHON(R"(
+        class A(object):
+            class B(object):
+                class C(object):
+                    pass
+    )");
+    ASSERT_TRUE(ns);
+
+    PyObject* A = PyDict_GetItemString(ns.get(), "A");
+    ASSERT_TRUE(A);
+
+    // attempt to access a few fields past the end of the real attribute chain.
+    py::scoped_ref<> actual = py::nested_getattr(A, "B", "C", "D", "E");
+    ASSERT_FALSE(actual);
+    expect_pyerr_type_and_message(PyExc_AttributeError,
+                                  "type object 'C' has no attribute 'D'");
+    PyErr_Clear();
+    // compare them using object identity
+}
+}  // namespace test_getattr
