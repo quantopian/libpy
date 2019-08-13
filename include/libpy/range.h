edb0315999104a01ada21d97c3e624ed4c537858
Joe Jevnik <joe@quantopian.com>
2019-08-13 18:51:16 -0400

    ENH: add a py::range function for iterating Python objects

---
+++ include/libpy/range.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <utility>
+
+#include "libpy/detail/python.h"
+#include "libpy/exception.h"
+#include "libpy/scoped_ref.h"
+
+namespace py {
+/** A range which lazily iterates over the elements of a Python iterable.
+ */
+class range {
+private:
+    py::scoped_ref<> m_iterator;
+
+    class iterator {
+    public:
+        using value_type = py::scoped_ref<>;
+        using reference = value_type&;
+
+    private:
+        PyObject* m_iterator;
+        value_type m_value;
+
+    public:
+        inline iterator() : m_iterator(nullptr), m_value(nullptr) {}
+
+        explicit iterator(PyObject* it);
+
+        iterator(const iterator&) = default;
+        iterator& operator=(const iterator&) = default;
+
+        reference operator*();
+        value_type* operator->();
+
+        iterator& operator++();
+        iterator operator++(int);
+
+        bool operator!=(const iterator& other) const ;
+        bool operator==(const iterator& other) const;
+    };
+
+public:
+    explicit range(PyObject* iterable);
+    inline explicit range(const py::scoped_ref<>& iterable) : range(iterable.get()) {}
+
+    iterator begin() const;
+    iterator end() const;
+};
+}  // namespace py
---
+++ src/range.cc
@@ -0,0 +1,53 @@
+#include "libpy/range.h"
+
+namespace py {
+range::iterator::iterator(PyObject* it) : m_iterator(it), m_value(nullptr) {
+    ++(*this);
+}
+
+range::iterator::reference range::iterator::operator*() {
+    return m_value;
+}
+
+range::iterator::value_type* range::iterator::operator->() {
+    return &m_value;
+}
+
+range::iterator& range::iterator::operator++() {
+    m_value = py::scoped_ref(PyIter_Next(m_iterator));
+    if (!m_value) {
+        if (PyErr_Occurred()) {
+            throw py::exception{};
+        }
+        m_iterator = nullptr;
+    }
+    return *this;
+}
+
+range::iterator range::iterator::operator++(int) {
+    range::iterator out = *this;
+    return ++out;
+}
+
+bool range::iterator::operator!=(const iterator& other) const {
+    return !(*this == other);
+}
+
+bool range::iterator::operator==(const iterator& other) const {
+    return m_iterator == other.m_iterator && m_value.get() == other.m_value.get();
+}
+
+range::range(PyObject* iterable) : m_iterator(PyObject_GetIter(iterable)) {
+    if (!m_iterator) {
+        throw py::exception{};
+    }
+}
+
+range::iterator range::begin() const {
+    return range::iterator{m_iterator.get()};
+}
+
+range::iterator range::end() const {
+    return range::iterator{};
+}
+}  // namespace py
