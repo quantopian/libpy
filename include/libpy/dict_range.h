5ba18e7bcfb735a198a4536a99d5f2ce60b57182
Joe Jevnik <joe@quantopian.com>
2019-08-13 18:51:42 -0400

    MAINT: move dict_range implementation to dict_range.cc

--- include/libpy/dict_range.h
+++ include/libpy/dict_range.h
@@ -24,69 +24,31 @@ private:
         value_type m_item;
 
     public:
-        iterator() : m_map(nullptr), m_pos(-1), m_item(nullptr, nullptr) {}
+        inline iterator() : m_map(nullptr), m_pos(-1), m_item(nullptr, nullptr) {}
 
-        explicit iterator(PyObject* map) : m_map(map), m_pos(0) {
-            ++(*this);
-        }
+        explicit iterator(PyObject* map);
+
         iterator(const iterator&) = default;
         iterator& operator=(const iterator&) = default;
 
-        reference operator*() {
-            return m_item;
-        }
+        reference operator*();
+        value_type* operator->();
 
-        value_type* operator->() {
-            return &m_item;
-        }
+        iterator& operator++();
+        iterator operator++(int);
 
-        iterator& operator++() {
-            if (!PyDict_Next(m_map, &m_pos, &m_item.first, &m_item.second)) {
-                m_map = nullptr;
-                m_pos = -1;
-                m_item.first = nullptr;
-                m_item.second = nullptr;
-            }
-            return *this;
-        }
-
-        iterator operator++(int) {
-            iterator out = *this;
-            return ++out;
-        }
-
-        bool operator!=(const iterator& other) const {
-            return m_pos != other.m_pos;
-        }
-
-        bool operator==(const iterator& other) const {
-            return m_pos == other.m_pos;
-        }
+        bool operator!=(const iterator& other) const ;
+        bool operator==(const iterator& other) const;
     };
 
 public:
-    explicit dict_range(PyObject* map) : m_map(map) {}
-    explicit dict_range(const py::scoped_ref<>& map) : dict_range(map.get()) {}
+    inline explicit dict_range(PyObject* map) : m_map(map) {}
+    inline explicit dict_range(const py::scoped_ref<>& map) : dict_range(map.get()) {}
 
-    static dict_range checked(PyObject* map) {
-        if (!PyDict_Check(map)) {
-            throw py::exception(PyExc_TypeError,
-                                "argument to py::dict_range::checked isn't a dict, got: ",
-                                Py_TYPE(map)->tp_name);
-        }
-        return dict_range(map);
-    }
+    static dict_range checked(PyObject* map);
+    static dict_range checked(const py::scoped_ref<>& map);
 
-    static dict_range checked(const py::scoped_ref<>& map) {
-        return checked(map.get());
-    }
-
-    iterator begin() const {
-        return iterator{m_map};
-    }
-
-    iterator end() const {
-        return iterator{};
-    }
+    iterator begin() const;
+    iterator end() const;
 };
 }  // namespace py
