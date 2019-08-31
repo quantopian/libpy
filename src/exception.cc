c0eed757e5159a787f0874866844db5098469610
Joe Jevnik <joe@quantopian.com>
2019-08-30 20:52:24 -0400

    ENH: nested exceptions
    Add support for nested exception messages being sent back to Python.
    Improves the error messages produced by `py::exception{}` calls when read from
    C++.

--- src/exception.cc
+++ src/exception.cc
@@ -1,9 +1,40 @@
 #include "libpy/exception.h"
 
 namespace py {
+namespace {
+void deep_what_recursive_helper(std::string& out, const std::exception& e, int level) {
+    if (level) {
+        out.push_back('\n');
+    }
+    out.insert(out.end(), level * 2, ' ');
+    const char* what = e.what();
+    if (*what) {
+        if (level) {
+            out += "raised from: ";
+        }
+        out += what;
+    }
+    else {
+        out += "raised from exception with empty what()";
+    }
+    try {
+        std::rethrow_if_nested(e);
+    }
+    catch (const std::exception& next) {
+        deep_what_recursive_helper(out, next, level + 1);
+    }
+}
+
+std::string deep_what(const std::exception& e) {
+    std::string out;
+    deep_what_recursive_helper(out, e, 0);
+    return out;
+}
+}  // namespace
+
 std::nullptr_t raise_from_cxx_exception(const std::exception& e) {
     if (!PyErr_Occurred()) {
-        py::raise(PyExc_RuntimeError) << "a C++ exception was raised: " << e.what();
+        py::raise(PyExc_RuntimeError) << "a C++ exception was raised: " << deep_what(e);
         return nullptr;
     }
     PyObject* type;
@@ -14,13 +45,34 @@ std::nullptr_t raise_from_cxx_exception(const std::exception& e) {
     Py_XDECREF(tb);
     const char* what = e.what();
     if (!what[0]) {
-        raise(type) << value << " (raised from C++ exception)";
+        raise(type) << value << "; raised from C++ exception";
     }
     else {
-        raise(type) << value << " (raised from C++ exception: " << what << ')';
+        raise(type) << value << "; raised from C++ exception: " << deep_what(e);;
     }
     Py_DECREF(type);
     Py_DECREF(value);
     return nullptr;
+}
+
+std::string exception::msg_from_current_pyexc() {
+    PyObject* type;
+    PyObject* value;
+    PyObject* tb;
+    PyErr_Fetch(&type, &value, &tb);
+    PyErr_NormalizeException(&type, &value, &tb);
+
+    py::scoped_ref as_str(PyObject_Str(value));
+    std::string out;
+    if (!as_str) {
+        out = "<failed to convert Python message to C++ message>";
+    }
+    else {
+        out = reinterpret_cast<PyTypeObject*>(type)->tp_name;
+        out += ": ";
+        out += py::util::pystring_to_string_view(as_str);
+    }
+    PyErr_Restore(type, value, tb);
+    return out;
 }
 }  // namespace py
