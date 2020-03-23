520d9dedd9f0e37a356c672500ec6876c12a45b6
Gerry Manoim <gmanoim@quantopian.com>
2020-03-23 18:46:24 -0400

    back to the way things were

--- include/libpy/filesystem.h
+++ include/libpy/filesystem.h
@@ -17,6 +17,7 @@ template<typename T, bool cond>
 constexpr bool defer_check = cond;
 }  // namespace detail
 
+#if PY_VERSION_HEX >= 0x03060000
 /** Get an std::filesystem::path from an object implementing __fspath__
 
     @param ob Object implementing __fspath__.
@@ -25,28 +26,27 @@ constexpr bool defer_check = cond;
 // make this a template to defer the static_assert check until the function is
 // used.
 template<typename T = void>
-std::filesystem::path fs_path(py::borrowed_ref<> ob) {
-    if constexpr (PY_VERSION_HEX >= 0x03060000) {
-        py::scoped_ref path_obj{PyOS_FSPath(ob.get())};
+std::filesystem::path fs_path([[maybe_unused]] py::borrowed_ref<> ob) {
+    #if PY_VERSION_HEX < 0x03060000
+    static_assert(detail::defer_check<T, (PY_VERSION_HEX >= 0x03060000)>,
+                  "cannot use fs_path in Python older than 3.6");
+    #else
+    py::scoped_ref path_obj{PyOS_FSPath(ob.get())};
 
-        if (!path_obj) {
-            throw py::exception{};
-        }
-        // depending on the implementation of __fspath__ we can get str or bytes
-        std::filesystem::path path;
-        if (PyBytes_Check(path_obj)) {
-            path = py::from_object<std::string>(path_obj);
-        }
-        else {
-            path = py::util::pystring_to_string_view(path_obj);
-        }
-
-        return path;
+    if (!path_obj) {
+        throw py::exception{};
+    }
+    // depending on the implementation of __fspath__ we can get str or bytes
+    std::filesystem::path path;
+    if (PyBytes_Check(path_obj)) {
+        path = py::from_object<std::string>(path_obj);
     }
     else {
-        static_assert(detail::defer_check<T, (PY_VERSION_HEX >= 0x03060000)>,
-                      "cannot use fs_path in Python older than 3.6");
+        path = py::util::pystring_to_string_view(path_obj);
     }
+
+    return path;
+    #endif
 }
 
 }  // namespace py::filesystem
