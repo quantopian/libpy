3a95d8695b52941035af511c17c75d05ae5bc5ee
Gerry Manoim <gmanoim@quantopian.com>
2020-03-23 18:39:18 -0400

    Use if constexpr

--- include/libpy/filesystem.h
+++ include/libpy/filesystem.h
@@ -17,7 +17,6 @@ template<typename T, bool cond>
 constexpr bool defer_check = cond;
 }  // namespace detail
 
-#if PY_VERSION_HEX >= 0x03060000
 /** Get an std::filesystem::path from an object implementing __fspath__
 
     @param ob Object implementing __fspath__.
@@ -25,24 +24,30 @@ constexpr bool defer_check = cond;
 */
 // make this a template to defer the static_assert check until the function is
 // used.
+template<typename T = void>
 std::filesystem::path fs_path(py::borrowed_ref<> ob) {
+    if constexpr(PY_VERSION_HEX >= 0x03060000) {
+        py::scoped_ref path_obj{PyOS_FSPath(ob.get())};
 
-    py::scoped_ref path_obj{PyOS_FSPath(ob.get())};
+        if (!path_obj) {
+            throw py::exception{};
+        }
+        // depending on the implementation of __fspath__ we can get str or bytes
+        std::filesystem::path path;
+        if (PyBytes_Check(path_obj)) {
+            path = py::from_object<std::string>(path_obj);
+        }
+        else {
+            path = py::util::pystring_to_string_view(path_obj);
+        }
 
-    if (!path_obj) {
-        throw py::exception{};
-    }
-    // depending on the implementation of __fspath__ we can get str or bytes
-    std::filesystem::path path;
-    if (PyBytes_Check(path_obj)) {
-        path = py::from_object<std::string>(path_obj);
+        return path;
     }
     else {
-        path = py::util::pystring_to_string_view(path_obj);
+        static_assert(detail::defer_check<T, (PY_VERSION_HEX >= 0x03060000)>,
+                  "cannot use fs_path in Python older than 3.6");
     }
-
-    return path;
 }
-#endif
+
 
 }  // namespace py::filesystem
