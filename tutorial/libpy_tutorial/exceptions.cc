a5376e4594cb1b42c4bf94004f29fcd770dc140c
Gerry Manoim <gmanoim@quantopian.com>
2020-06-16 11:49:21 -0400

    ENH add some exceptions

--- tutorial/libpy_tutorial/exceptions.cc
+++ tutorial/libpy_tutorial/exceptions.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdexcept>
 #include <vector>
 
 #include <libpy/abi.h>
@@ -19,10 +20,21 @@ void throw_value_error(int a) {
     throw py::exception(PyExc_ValueError, "You passed ", a, " and this is the exception");
 }
 
+void raise_from_cxx() {
+    try {
+        throw std::invalid_argument("Supposedly a bad argument was used");
+    }
+    catch (std::invalid_argument& e) {
+        py::raise_from_cxx_exception(e);
+        throw py::exception{};
+    }
+}
+
 namespace {
 PyMethodDef methods[] = {
     py::autofunction<raise_a_value_error>("raise_a_value_error"),
     py::autofunction<throw_value_error>("throw_value_error"),
+    py::autofunction<raise_from_cxx>("raise_from_cxx"),
     py::end_method_list,
 };
 
