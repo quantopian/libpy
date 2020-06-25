9182ad1139e5ab74f8de0b7d4509059b3b48e13b
Joe Jevnik <joe@quantopian.com>
2020-06-25 19:59:22 -0400

    MAINT: remove vestigial files

--- include/libpy/py2.h
+++
@@ -1,9 +0,0 @@
-#include "libpy/detail/python.h"
-
-#if PY_MAJOR_VERSION == 2
-#define Py_RETURN_NOTIMPLEMENTED                                                         \
-    Py_INCREF(Py_NotImplemented);                                                        \
-    return Py_NotImplemented;
-
-using Py_hash_t = long;
-#endif
